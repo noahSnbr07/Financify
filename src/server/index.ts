37e912dfec144a41ace1d9431017a976bd5fdb48
noah.snbr <nur.noah.saschenbrecker@gmail.com>
2026-08-18 21:44:34 +0200

    updated configurtation files and proxy

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,6 @@ import getCategoryVolume from "./get-category-volume";
 import getTotalAccountVolume from "./get-total-account-volume";
 import getUserDatabaseStats from "./get-user-database-stats";
 import getDashboardData from "./get-dashboard-data";
-import getForecast from "./get-forecast";
 import getOllamaHost from "./get-ollama-host";
 
 export {
@@ -14,6 +13,5 @@ export {
     getTotalAccountVolume,
     getUserDatabaseStats,
     getDashboardData,
-    getForecast,
     getOllamaHost
 }

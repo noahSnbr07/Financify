db492ca8188f4f483812a36fe03339376169fa01
noah.snbr <nur.noah.saschenbrecker@gmail.com>
2026-08-27 01:49:31 +0200

    warning system flags transactio ncreation without account/category

---
+++ src/static/warning.ts
@@ -0,0 +1,26 @@
+
+
+export interface WarningProps {
+    id: number;
+    href: string;
+    body: string;
+    label: string;
+}
+
+const warnings: { NO_CATEGORIES: WarningProps; NO_ACCOUNTS: WarningProps; } = {
+    NO_CATEGORIES: {
+        id: 0,
+        href: "/categories/new",
+        body: "In order to create a Transaction you must first create a category.",
+        label: "Create Category",
+    },
+    NO_ACCOUNTS: {
+        id: 1,
+        href: "/accounts/new",
+        body: "In order to create a Transaction you must first create an account.",
+        label: "Create Account",
+    }
+
+}
+
+export default warnings;
